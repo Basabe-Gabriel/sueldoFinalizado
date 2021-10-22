1995d9b2018d3027cb7982002453c368332003de
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-22 12:49:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let nota: number = 0;
+let promedio: number = 0;
+let contador: number = 1;
+let suma: number = 0;
+
+while (contador <= 10) {
+  nota = Number  (prompt("ingrese la nota");
+
+  suma = suma + nota;
+  contador = contador + 1;
+}
+promedio = suma / 10;
+
+console.log("el promedio es: " + promedio);
+console.log("la suma es:" + suma);
